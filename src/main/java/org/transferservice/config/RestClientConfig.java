8fa7353135d860035ce7ed3cbc3e058da0ddcade
ilovechocolates05 <109774245+ilovechocolates05@users.noreply.github.com>
2026-08-30 19:54:25 -0500

    feat: add service timeouts

--- src/main/java/org/transferservice/config/RestClientConfig.java
+++ src/main/java/org/transferservice/config/RestClientConfig.java
@@ -3,15 +3,25 @@ package org.transferservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
     public RestClient accountRestClient() {
+
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+
+        requestFactory.setReadTimeout(Duration.ofSeconds(2));
+        requestFactory.setConnectTimeout(Duration.ofSeconds(2));
+
         return RestClient.builder()
                 .baseUrl("http://localhost:8082")
+                .requestFactory(requestFactory)
                 .build();
     }
 
